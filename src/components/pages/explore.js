0df81e1bd4ac28a0b8db16ac96415097db864ad0
Shahir <125945242+shahirsiraj@users.noreply.github.com>
2023-09-02 09:26:53 +0800

    Update explore.js

--- src/components/pages/explore.js
+++ src/components/pages/explore.js
@@ -1,4 +1,4 @@
-
+ 
 
 import axios from "axios"
 import { useState, useEffect } from "react"
@@ -213,7 +213,7 @@ export default function Explore() {
                 className="text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none  bg-black text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart h-12 lg:h-14 mt-1 text-sm lg:text-base w-full sm:w-auto"
                 type="submit"
               >
-                Send Message
+                Submit!
               </button>
             </div>
           </div>
